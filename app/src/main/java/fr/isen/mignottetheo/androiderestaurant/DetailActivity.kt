91ad0a3fafe1ccf2fd6b1de605c18d08458c9e6d
tutoy83 <theo.mignotte@isen.yncrea.fr>
2023-01-29 17:55:06 +0100

    Add checkout on button click

--- app/src/main/java/fr/isen/mignottetheo/androiderestaurant/DetailActivity.kt
+++ app/src/main/java/fr/isen/mignottetheo/androiderestaurant/DetailActivity.kt
@@ -2,10 +2,15 @@ package fr.isen.mignottetheo.androiderestaurant
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
 import fr.isen.mignottetheo.androiderestaurant.databinding.ActivityDetailBinding
 import fr.isen.mignottetheo.androiderestaurant.model.Items
 import com.squareup.picasso.Picasso
+import com.google.gson.Gson
 import fr.isen.mignottetheo.androiderestaurant.databinding.ActivityMealBinding
+import java.io.File
+import java.io.FileOutputStream
+
 @Suppress("DEPRECATION")
 
 class DetailActivity : AppCompatActivity() {
@@ -20,7 +25,6 @@ class DetailActivity : AppCompatActivity() {
         //Recuperer le menu choisi
         val meal = intent.getSerializableExtra("mealselected") as Items
 
-
         //Modifier le titre en fct de la cat seletionee
         //val itemtodisplay = intent.getStringExtra("detailItemName")
         binding.detailTitleArticle.text = meal?.nameFr
@@ -30,7 +34,6 @@ class DetailActivity : AppCompatActivity() {
         if (mealSelectedPic.isNotEmpty()) {
             Picasso.get().load(mealSelectedPic).into(binding.detailImageArticle)
         }
-
 
         var currentQuantity = binding.quantityNumber.text.toString().toInt()
 
@@ -51,6 +54,8 @@ class DetailActivity : AppCompatActivity() {
             updateTotal(meal, currentQuantity)
         }
 
+        var totalPrice = 0
+
         //diminuer la quantite sur click du bouton
         binding.buttonDetailMinus.setOnClickListener {
             currentQuantity -= 1
@@ -60,8 +65,7 @@ class DetailActivity : AppCompatActivity() {
             }
             // update le TextView de la qte
             binding.quantityNumber.text = currentQuantity.toString()
-            updateTotal(meal, currentQuantity)
-
+            totalPrice = updateTotal(meal, currentQuantity)
         }
 
 
@@ -70,20 +74,41 @@ class DetailActivity : AppCompatActivity() {
         val ingredientsString = ingredientss.joinToString(", ")
         binding.contentMealDetails.text = ingredientsString
 
+        binding.totalPrice.setOnClickListener {
+            // create the object that holds the meal information
+            val mealInfo = MealInfo(meal.nameFr.toString(), currentQuantity, totalPrice)
+            // convert the object to JSON format
+            val jsonData = Gson().toJson(mealInfo)
+            // write the JSON data to a file
+            val file = File(applicationContext.filesDir, "panier.json")
+            FileOutputStream(file).apply {
+                write(jsonData.toByteArray())
+                close()
+            }
+            // show a Snackbar or AlertDialog to inform the user that the meal has been added to the cart
+            Snackbar.make(binding.root, "Meal added to cart", Snackbar.LENGTH_SHORT).show()
 
+        }
     }
 
-    private fun updateTotal(meal:Items, currentQuantity: Int) {
+    private fun updateTotal(meal:Items, currentQuantity: Int): Int {
         val prices = meal.prices
-        if (prices.isNotEmpty()) {
-            val mealSelectedPrice = prices[0]
-            var priceValue = mealSelectedPrice.price.toString().toInt() * currentQuantity
+        val mealSelectedPrice = prices[0]
+        var priceValue = mealSelectedPrice.price.toString().toInt() * currentQuantity
 
+        if (prices.isNotEmpty()) {
             //securite valeur negative
             if(priceValue<0){
                 priceValue = 0
             }
             binding.totalPrice.text = "Total ${priceValue} €"
+
         }
+        return priceValue
     }
+    data class MealInfo(
+        val name: String,
+        val quantity: Int,
+        val priceTotal: Int
+        )
 }
